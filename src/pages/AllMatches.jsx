648c6010631ef3b406248c3ea88a14d7265ddf4a
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-11-07 11:59:07 +0000

    managed to get all customer2 uids and add them to an array

--- src/pages/AllMatches.jsx
+++ src/pages/AllMatches.jsx
@@ -1,50 +1,113 @@
 
 import {useEffect, useState} from 'react'
 import { getAuth} from 'firebase/auth'
-import { getDoc,doc} from 'firebase/firestore'
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+  documentId
+} from 'firebase/firestore'
 import { db } from '../firebase.config'
 import {MapContainer, Marker, TileLayer} from 'react-leaflet'
+import { toast } from 'react-toastify'
+
 
 function AllMatches() {
 
     const [currentUser, setCurrentUser] = useState(null)
     const [matches, setMatches] = useState([]);
     const [selectedMatch, setSelectedMatch] = useState(null);
+    const [firebaseReturn, setFirebaseReturn] = useState(null)
     // const [matchesFromCustomer1View, setMatchesFromCustomer1View] = useState([])
 
 
             //get firebaseID
             const auth = getAuth()
 
-        useEffect(() => {
+
+        useEffect(() =>  {
           getAllMatches()
           setCurrentUser(auth.currentUser.uid)
-          }, 
-          [])
+          createMatchesUidArray()
+          
+          const fetchFirebaseMatches = async () => {
+            try {
+              // get Reference
+              const firebaseMatchesRef = collection(db, 'users')
+      
+              // create a query
+              const buildQuery = query(
+                firebaseMatchesRef,
+                where(documentId(), "in", 
+                matchesUids
+              ),
+                // orderBy('timestamp', 'desc', limit(10))
+              )
+              // execute query
+      
+              const querySnap = await getDocs(buildQuery)
+      
+              const firebaseMatchesTemp = []
+      
+              querySnap.forEach((doc) => {
+                return firebaseMatchesTemp.push({
+                  id: doc.id,
+                  data: doc.data(),
+                })
+              })
+              setFirebaseReturn(firebaseMatchesTemp)
+
+            } catch (error) {
+              toast.error('Could not fetch listings')
+            }
+          }
+          fetchFirebaseMatches()
+        }, [])
+
 
         const getAllMatches = async () => {
         await fetch(`https://woofingfromhome.herokuapp.com/matches?firebaseId=${auth.currentUser.uid}`)
         .then(res => res.json())
-        .then(matches => setMatches(matches))  
+        .then(matches => setMatches(matches))
+        // .then(createMatchesUidArray())
         }
         
-          const matchesFromCustomer1View = matches.filter(match =>
-            match.customer1.firebaseId == currentUser && match.score > 0)
+        const matchesUids = []
+
+        const createMatchesUidArray = () => {
+          matches.slice(0, 10).map(match => {
+            if (match.customer2.firebaseId !== currentUser.firebaseId){
+              matchesUids.push(match.customer2.firebaseId)
+            }
+          }
+          )
+        }
+
+        // createMatchesUidArray()
+        console.log(matchesUids)
+
+        
+        const matchesFromCustomer1View = matches.filter(match =>
+        match.customer1.firebaseId == currentUser && match.score > 0)
 
                     
         // for each customer 2 in matchesFromCustomer1View go to firebase and get the name, about me and image and add to customer 2 object in matchesFromCustomer1View
 
-            useEffect(() => {
-              const getFirebaseInfo = () => {
-                matchesFromCustomer1View.forEach(match => {
-                  const docRef = doc(db, 'users', 'kF1hjQIgdKer8CMqMO1jRAQsiGM2')
-                  const docSnap = getDoc(docRef)
-                  .then(docSnap => 
-                    match.customer2.name = docSnap.data().name)                                
-                  }             
-              )}
-              getFirebaseInfo()
-            })
+            // useEffect(() => {
+            //   const getFirebaseInfo = () => {
+            //     matchesFromCustomer1View.forEach(match => {
+            //       const docRef = doc(db, 'users', 'kF1hjQIgdKer8CMqMO1jRAQsiGM2')
+            //       const docSnap = getDoc(docRef)
+            //       .then(docSnap => 
+            //         match.customer2.name = docSnap.data().name)                                
+            //       }             
+            //   )}
+            //   getFirebaseInfo()
+            // })
 
     //access score from other persons perspective
 
@@ -81,8 +144,8 @@ function AllMatches() {
           <ul
             className="space-y-12 sm:-mt-8 sm:space-y-0 sm:divide-y sm:divide-gray-200 lg:gap-x-8 lg:space-y-0"
           >
-            {matchesFromCustomer1View.map((match) => (
-              <li key={match.customer2.firebaseId} className="sm:py-8">
+            {matchesFromCustomer1View.map((match, index) => (
+              <li key={index} className="sm:py-8">
                 <div className="space-y-4 sm:grid sm:grid-cols-3 sm:items-start sm:gap-6 sm:space-y-0">
                   <div className="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
                     {/* <img className="rounded-lg object-cover shadow-lg" src={person.imageUrl} alt="" /> */}
@@ -103,7 +166,7 @@ function AllMatches() {
                             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
                             />
-                        <Marker position={[match.customer2.latitude, match.customer2. longitude]}>
+                        <Marker position={[match.customer2.latitude, match.customer2.longitude]}>
                         </Marker>
                     </MapContainer>
                     </div>
@@ -117,7 +180,7 @@ function AllMatches() {
                       </div>
                       <ul className="flex space-x-5">
                         <li>
-                          <a href='/' className="text-gray-400 hover:text-gray-500">
+                          <a href='/matches/{match.id}' className="text-gray-400 hover:text-gray-500">
                             Take a look at their profile</a>
                         </li>
                         <li>

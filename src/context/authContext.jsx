72e0793f8a50d3b9c53e50433479e49acd02634b
Conrad Russo <conradr@gmail.com>
2022-11-08 10:43:32 +0000

    navbar improvements

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -6,19 +6,23 @@ export const AuthContext = createContext()
 export const AuthContextProvider = ({ children }) => {
   const auth = getAuth()
   const [currentUser, setCurrentUser] = useState({})
+  const [profileImage, setprofileImage] = useState()
+
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user)
+      setprofileImage(user.photoURL)
+     
     })
-
+   
     return () => {
       unsub()
     }
   }, [])
 
   return (
-    <AuthContext.Provider value={{ currentUser }}>
+    <AuthContext.Provider value={{ currentUser, profileImage }}>
       {children}
     </AuthContext.Provider>
   )

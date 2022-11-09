2b1711aa48acb73f8b6f53bbddb0b7e223979de8
Katie Booth <katiebooth3@gmail.com>
2022-11-09 11:17:17 +0000

    Updated background image on individual match page

--- src/pages/MatchProfile.jsx
+++ src/pages/MatchProfile.jsx
@@ -30,26 +30,6 @@ import {
 
 import { AuthContext } from "../context/authContext";
 
-const profile = { 
-
-
-  name: 'Ricardo Cooper',
-  email: 'ricardo.cooper@example.com',
-  avatar:
-    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-  backgroundImage:
-    'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
-  fields: [
-    ['Phone', '(555) 123-4567'],
-    ['Email', 'ricardocooper@example.com'],
-    ['Title', 'Senior Front-End Developer'],
-    ['Team', 'Product Development'],
-    ['Location', 'San Francisco'],
-    ['Sits', 'Oasis, 4th floor'],
-    ['Salary', '$145,000'],
-    ['Birthday', 'June 8, 1990'],
-  ],
-}
 
 const homeSetUp = [
   {
@@ -236,7 +216,7 @@ const MatchProfile = () => {
         <div>
           <img
             className='h-32 w-full object-cover lg:h-48'
-            src={profile.backgroundImage}
+            src="https://firebasestorage.googleapis.com/v0/b/woofing-from-home.appspot.com/o/images%2Fcover-image.png?alt=media&token=5fd70878-8af7-4288-909c-3341127a11de"
             alt=''
           />
         </div>

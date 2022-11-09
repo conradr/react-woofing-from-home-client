73310f414ab69cca542b706cb5709e91d093f150
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-11-09 14:07:47 +0000

    button changed

--- src/pages/SuitabilityForm.jsx
+++ src/pages/SuitabilityForm.jsx
@@ -1190,7 +1190,7 @@ function SuitabilityForm() {
                 className='inline-flex  rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
                 onClick={onSubmit}
               >
-                Next
+                Submit
               </button>
             </div>
           </form>

5be26362ee961684d3e0ea2097961b0853b194e1
Conrad Russo <conradr@gmail.com>
2022-11-09 20:33:34 +0000

    chat styling

--- src/components/chatComponents/ChatDesign.jsx
+++ src/components/chatComponents/ChatDesign.jsx
@@ -144,7 +144,11 @@ const ChatDesign = () => {
                     </svg>
                   </button>
                 </div>
+                
+                
+                
               </div>
+
               <div
                 id='messages'
                 class='flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'

925036147b6f8282d593066dbdeedf4325cadcfa
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-11-09 11:42:21 +0000

    working nav menu for mobile

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,75 +1,74 @@
-import { useContext, useState } from 'react'
-import { Fragment } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { AuthContext } from '../context/authContext'
-import boneLogo from '../assets/svg/boneLogo.svg'
+import { useContext, useState } from "react";
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { AuthContext } from "../context/authContext";
+import boneLogo from "../assets/svg/boneLogo.svg";
 
 const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Matches', href: '/matches', current: false },
-  { name: 'About Us', href: '/about', current: false },
-  { name: 'FAQ', href: '/faq', current: false },
-  { name: 'Contact Us', href: '/contactus', current: false}
-]
+  { name: "Home", href: "/", current: true },
+  { name: "Matches", href: "/matches", current: false },
+  { name: "About Us", href: "/about", current: false },
+  { name: "FAQ", href: "/faq", current: false },
+  { name: "Contact Us", href: "/contactus", current: false },
+];
 const userNavigation = [
-  { name: 'Your Profile', href: '/profile' },
-  { name: 'Sign out', href: '/', onClick: `onLogout` },
-  { name: 'Log In', href: '/log-in' },
-]
+  { name: "Your Profile", href: "/profile" },
+  { name: "Sign out", href: "/", onClick: `onLogout` },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const HeaderNav = () => {
-  const { currentUser, profileImage } = useContext(AuthContext)
+  const { currentUser, profileImage } = useContext(AuthContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const navigateToProfile = () => {
-    navigate('/profile')
-  }
+    navigate("/profile");
+  };
 
   const onLogout = () => {
-    currentUser.auth.signOut()
-    navigate('/')
-  }
+    currentUser.auth.signOut();
+    navigate("/");
+  };
   const onSignin = () => {
-    navigate('/sign-in')
-  }
+    navigate("/sign-in");
+  };
 
   return (
     <>
-      <Disclosure as='nav' className='bg-gray-800'>
+      <Disclosure as="nav" className="bg-gray-800">
         {({ open }) => (
           <>
-            <div className='mx-auto max-w-7xl sm:px-8 lg:px-12'>
-              <div className='border-b border-gray-700'>
-                <div className='flex h-16 items-center justify-between px-4 sm:px-0'>
-                  <div className='flex items-center flex-shrink-0'>
-                    <a href='/' className='flex items-center flex-shrink-0'>
+            <div className="mx-auto max-w-7xl sm:px-8 lg:px-12">
+              <div className="border-b border-gray-700">
+                <div className="flex h-16 items-center justify-between px-4 sm:px-0">
+                  <div className="flex items-center flex-shrink-0">
+                    <a href="/" className="flex items-center flex-shrink-0">
                       <img
-                        className='h-12 w-12  px-2'
+                        className="h-12 w-12  px-2"
                         src={boneLogo}
-                        alt='woofing from home logo'
+                        alt="woofing from home logo"
                       />
-                      <span className='text-gray-300'>Woofing from home</span>
+                      <span className="text-gray-300">Woofing from home</span>
                     </a>
-                    <div className='hidden md:block'>
-                      <div className='ml-10 flex items-baseline space-x-4'>
+                    <div className="hidden md:block">
+                      <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
                           <a
                             key={item.name}
                             href={item.href}
                             className={classNames(
                               item.current
-                                ? 'bg-gray-900 text-white'
-                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                              'px-3 py-2 rounded-md text-sm font-medium'
+                                ? "bg-gray-900 text-white"
+                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                              "px-3 py-2 rounded-md text-sm font-medium"
                             )}
-                            aria-current={item.current ? 'page' : undefined}
+                            aria-current={item.current ? "page" : undefined}
                           >
                             {item.name}
                           </a>
@@ -77,32 +76,32 @@ const HeaderNav = () => {
                       </div>
                     </div>
                   </div>
-                  <div className='flex items-center'>
-                    <div className='hidden md:block'>
-                      <div className='ml-4 flex items-center md:ml-6'>
+                  <div className="flex items-center">
+                    <div className="hidden md:block">
+                      <div className="ml-4 flex items-center md:ml-6">
                         <button
-                          type='button'
-                          className='rounded-full bg-gray-800 p-1 text-gray-400 focus:ring-offset-gray-800'
+                          type="button"
+                          className="rounded-full bg-gray-800 p-1 text-gray-400 focus:ring-offset-gray-800"
                         >
-                          <span className='sr-only'>View notifications</span>
+                          <span className="sr-only">View notifications</span>
                         </button>
 
                         {/* Profile dropdown */}
-                        <Menu as='div' className='relative ml-3'>
+                        <Menu as="div" className="relative ml-3">
                           <div>
                             {currentUser ? (
-                              <Menu.Button className='flex max-w-xs items-center rounded-full bg-gray-800 text-sm'>
-                                <span className='sr-only'>Open user menu</span>
+                              <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm">
+                                <span className="sr-only">Open user menu</span>
                                 <img
-                                  className='h-12 w-12 rounded-full'
+                                  className="h-12 w-12 rounded-full"
                                   src={profileImage}
-                                  alt=''
+                                  alt=""
                                 />
                               </Menu.Button>
                             ) : (
                               <button
-                                type='button'
-                                className='inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                                type="button"
+                                className="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                 onClick={onSignin}
                               >
                                 Sign in
@@ -111,21 +110,21 @@ const HeaderNav = () => {
                           </div>
                           <Transition
                             as={Fragment}
-                            enter='transition ease-out duration-100'
-                            enterFrom='transform opacity-0 scale-95'
-                            enterTo='transform opacity-100 scale-100'
-                            leave='transition ease-in duration-75'
-                            leaveFrom='transform opacity-100 scale-100'
-                            leaveTo='transform opacity-0 scale-95'
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
                           >
-                            <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                               <Menu.Item>
                                 {({ active }) => (
                                   <button
                                     onClick={navigateToProfile}
                                     className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
+                                      active ? "bg-gray-100" : "",
+                                      "block px-4 py-2 text-sm text-gray-700"
                                     )}
                                   >
                                     {userNavigation[0].name}
@@ -137,8 +136,8 @@ const HeaderNav = () => {
                                   <button
                                     onClick={onLogout}
                                     className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
+                                      active ? "bg-gray-100" : "",
+                                      "block px-4 py-2 text-sm text-gray-700"
                                     )}
                                   >
                                     {userNavigation[1].name}
@@ -150,19 +149,19 @@ const HeaderNav = () => {
                         </Menu>
                       </div>
                     </div>
-                    <div className='-mr-2 flex md:hidden'>
+                    <div className="-mr-2 flex md:hidden">
                       {/* Mobile menu button */}
-                      <Disclosure.Button className='inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 focus:ring-offset-gray-800'>
-                        <span className='sr-only'>Open main menu</span>
+                      <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 focus:ring-offset-gray-800">
+                        <span className="sr-only">Open main menu</span>
                         {open ? (
                           <XMarkIcon
-                            className='block h-6 w-6'
-                            aria-hidden='true'
+                            className="block h-6 w-6"
+                            aria-hidden="true"
                           />
                         ) : (
                           <Bars3Icon
-                            className='block h-6 w-6'
-                            aria-hidden='true'
+                            className="block h-6 w-6"
+                            aria-hidden="true"
                           />
                         )}
                       </Disclosure.Button>
@@ -171,45 +170,55 @@ const HeaderNav = () => {
                 </div>
               </div>
             </div>
-            <Disclosure.Panel className='border-b border-gray-700 md:hidden'>
-              <div className='space-y-1 px-2 py-3 sm:px-3'>
+            <Disclosure.Panel className="border-b border-gray-700 md:hidden">
+              <div className="space-y-1 px-2 py-3 sm:px-3">
                 {navigation.map((item) => (
                   <Disclosure.Button
                     key={item.name}
-                    as='a'
+                    as="a"
                     href={item.href}
                     className={classNames(
                       item.current
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'block px-3 py-2 rounded-md text-base font-medium'
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "block px-3 py-2 rounded-md text-base font-medium"
                     )}
-                    aria-current={item.current ? 'page' : undefined}
+                    aria-current={item.current ? "page" : undefined}
                   >
                     {item.name}
                   </Disclosure.Button>
                 ))}
               </div>
-              <div className='border-t border-gray-700 pt-4 pb-3'>
-                <div className='mt-3 space-y-1 px-2'>
-                  {userNavigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as='a'
-                      href={item.href}
-                      className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
+              {currentUser ? (
+                <div className="border-t border-gray-700 pt-4 pb-3">
+                  <div className="mt-3 space-y-1 px-2">
+                    {userNavigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    ))}
+                  </div>
                 </div>
-              </div>
+              ) : (
+                <button
+                  type="button"
+                  className="rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mx-4"
+                  onClick={onSignin}
+                >
+                  Sign in
+                </button>
+              )}
             </Disclosure.Panel>
           </>
         )}
       </Disclosure>
     </>
-  )
-}
+  );
+};
 
-export default HeaderNav
+export default HeaderNav;
